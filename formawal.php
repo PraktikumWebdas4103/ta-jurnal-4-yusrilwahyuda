725f2f164ba2796d7f8b26d83510939444e90a42
yusrilwahyuda <43510262+yusrilwahyuda@users.noreply.github.com>
2018-09-27 00:15:02 +0800

    Update formawal.php

--- formawal.php
+++ formawal.php
@@ -1,5 +1,35 @@
+<form  method="POST">
+	<table>
+	<tr>
+		<td>Nama</td><td><input type="text" name="nama"></td>
+	</tr>
+	<tr>
+		<td>Password</td><td><input type="password" name="password"></td>
+	</tr>
+	<tr>
+		<td><input type="submit" name="submit" value="submit"></td>
+	</tr>
+</table>
+</form>
+
 <?php
+session_start();
 
+$data2 = array(
+			"username" => "yusril",
+			"password" => "yusril"
+	);
 
-
+	if(isset($_POST['submit'])){
+		$username = $_POST['nama'];
+		$pass = $_POST['password'];
+		
+		if ($username == $data2["username"] && $pass == $data2["password"]){
+			$_SESSION['username'] = $username;
+			header("location:proses.php");
+		}
+		else{
+			header("location:login.php");
+		}
+	}
 ?>
